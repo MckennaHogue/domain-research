610a9ba42148a09d3407658d69d604e54c6322b1
MckennaHogue <mckennahogue@gmail.com>
2015-04-09 16:38:28 -0400

    Homework!

--- mock-ups/html/spearfish/main.js
+++ mock-ups/html/spearfish/main.js
@@ -1,64 +1,229 @@
 
-var xPos= 200;
-var yPos= 400;
-var fishCount=0;
+// var xPos= 200;
+// var yPos= 400;
+// var waterSurface = 210;
+// var waterFloor = 600;
+// var fishCount=0;
 
-for(var fc = 0;  fc<10; fc++){
-  drawFish(Math.random()*800);
+// for(var fc = 0;  fc<10; fc++){
+//   drawFish(Math.random()*800);
+// }
+
+// function drawFish(xPosition){
+//   fishCount++;
+//   // var fishxPos= Math.random()*800;
+//   var fishyPos= 200+Math.random()*450;
+//   $("#game").append ('<img src="assets/fish1.gif" alt="fish" id="fish'+fishCount+'" class="fish">');
+//   $("#fish"+fishCount).css({  "top":fishyPos+"px", "left": xPosition+ "px"});
+//   swimleft("#fish"+fishCount, xPosition);
+//   console.log('x');
+// }
+
+// setInterval( function(){
+//   drawFish(0);
+// }, Math.random() *10000);
+
+// function swimleft(fish, fishPos){
+//   for(var x=0; x<1000; x++){
+//     fishPos+=10;
+//     $(fish).animate({"left": fishPos+"px"});
+//   }
+// }
+
+// $(document).keydown(function(e) {
+//   switch(e.which){
+//     case 37://left
+//     console.log("left");
+//     xPos-=10;
+//     //xpos=xpos-10
+//     break;
+
+//     case 39: //right
+//      xPos+=10;
+//     //xpos=xpos+10
+//     console.log("right");
+//     break;
+
+//     case 38://up
+//     yPos-=10;
+//     console.log("up");
+//     break;
+
+//     case 40: //down
+//     yPos+=10;
+//     console.log("down");
+//     break;
+
+//     default: return;
+//   }
+//   e.preventDefault();
+
+//   if(yPos < waterSurface){
+//     yPos = waterSurface;
+//   }
+
+//   if(yPos >waterFloor){
+//     yPos = waterFloor;
+//   }
+
+
+
+//   console.log("xPos:"+xPos+"  yPos:" + yPos);
+
+//   $("#diver").css({  "top":yPos+"px", "left": xPos+ "px"});
+// });
+
+
+
+
+// /*
+
+
+// // simulation
+// collection of fish (array)
+// start by making multiple fish and giving them random positions (populating an array)
+
+// for each item in the array - create an html element for the fish and set its position
+
+// diver
+// x
+// y
+
+
+// // representation / interface
+// render loop - runs every 60th of a second
+//   loop through all the fish and update their positions
+//   update the divers position
+
+
+// */
+
+
+
+/* Global Variables */
+
+var $game = $('#game')
+var waterSurface = 210;
+var waterFloor = 600;
+
+/* Seeding Our Data */
+
+var makeFish = function(startingX, startingY, startingSpeed){
+   return {
+    x: startingX,
+    y: startingY,
+    speed: startingSpeed
+   };
+};
+
+var schoolOfFish = [];
+
+for(var fc = 0; fc < 10; fc++){ // Adding 10 fish to schoolOfFish
+  var fish = makeFish(Math.random()*300, Math.random()*300, Math.random()*10); // x, y, speed
+  schoolOfFish.push(fish);
 }
-function drawFish(xPosition){
-  fishCount++;
-  // var fishxPos= Math.random()*800;
-  var fishyPos= 200+Math.random()*450;
-  $("#game").append ('<img src="assets/fish1.gif" alt="fish" id="fish'+fishCount+'" class="fish">');
-  $("#fish"+fishCount).css({  "top":fishyPos+"px", "left": xPosition+ "px"});
-  swimleft("#fish"+fishCount, xPosition);
 
+var diverobject = {
+  x: 150,
+  y: 300
 }
 
-setInterval( function(){
-  drawFish(0);
-  }, Math.random() *10000);
+$(document).keydown(function(e){ // from what i had above up and down keys
+  // if key is up
+  // diver.y -= 10;
 
-
-function swimleft(fish, fishPos){
-  for(var x=0; x<1000; x++){
-    fishPos+=10;
-    $(fish).animate({"left": fishPos+"px"})
-  }
-}
-
-$(document).keydown(function(e) {
-  switch(e.which){
+    switch(e.which){
     case 37://left
-    console.log("left")
-    xPos-=10;
+    console.log("left");
+    diverobject.x-=10;
     //xpos=xpos-10
     break;
 
-    case 38://up
-    yPos-=10;
-    console.log("up")
-
+    case 39: //right
+    diverobject.x+=10;
+    //xpos=xpos+10
+    console.log("right");
     break;
 
-    case 39: //right
-     xPos+=10;
-    //xpos=xpos+10
-    console.log("right")
-
+    case 38://up
+    diverobject.y-=10;
+    console.log("up");
     break;
 
     case 40: //down
-    yPos+=10;
-    console.log("down")
-
+    diverobject.y+=10;
+    console.log("down");
     break;
 
     default: return;
   }
+
   e.preventDefault();
 
-  console.log("xPos:"+xPos+"  yPos:" + yPos);
-  $("#diver").css({  "top":yPos+"px", "left": xPos+ "px"});
+  if(diverobject.y< waterSurface){
+    diverobject.y = waterSurface;
+  }
+
+  if(diverobject.y >waterFloor){
+    diverobject.y = waterFloor;
+  }
 });
+
+
+/* Game Loop */
+
+setInterval(function(){
+
+
+  /* Update Fish */
+  for(var i = 0; i < schoolOfFish.length; i++){ // Loop through our schoolOfFish
+
+    var currentFish = schoolOfFish[i];
+    var fishId = "fish"+i;
+    var fishElement = $("#"+fishId);
+
+    if(fishElement.length == 0){ // Add html elements for fish if they don't exist.
+       $game.append ('<img src="assets/fish1.gif" alt="fish" id="'+fishId+'" class="fish">');
+       fishElement = $("#"+fishId); // Updates fishElement in case the fish didn't exist.
+    }
+
+    currentFish.x += currentFish.speed;
+
+    if(currentFish.x >=1590){
+      console.log("kill the fish");
+      //play sound
+      var fishsound = new Audio("assets/0437.ogg");
+      fishsound.play();
+      currentFish.x =0;
+    }
+
+    // Updates the fish's css properties.
+    fishElement.css({  "top":currentFish.y+"px", "left": currentFish.x+ "px"});
+
+    // if(currentFish.x >1590) {
+    //   console.log("kill the fish");
+
+
+  }
+
+  /* Update Diver */
+  // TODO
+  var diver=$("#diver");
+  diver.css({  "top":diverobject.y+"px", "left": diverobject.x+ "px"});
+
+
+
+}, 16);
+
+
+//  For over spring break =
+//      make fish die after leave screen not go on forever
+//      add a sound maybe when fish get to the end of the page
+//      work on style of the page id: the directions, maybe making the game the whole page and directions up top
+//      document diver position? like how he did the fish?
+
+//    PLAY GAMES: think about how one key for the whole game/also never ending game.
+//    https://itunes.apple.com/us/app/desert-golfing/id902062673?mt=
+//    http://www.adamatomic.com/canabalt/
+
+// margine auto means it automatically goes in the center
